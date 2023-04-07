d23d832cd6a8ae36d840e8696bcf3d790832b13b
ToByDaNy <cacarazatobias@yahoo.com>
2023-04-08 00:34:55 +0300

    a1

--- a1/a1.c
+++ a1/a1.c
@@ -32,7 +32,6 @@ void ok(const char *path, bool *flagSUCCES, bool *flagLines)
     read(fp, &HEADER_SIZE, 2);
     read(fp, &VERSION, 4);
     read(fp, &NR_OF_SECTIONS, 1);
-    // printf("MAGIC=%c HEADER_SIZE=%hd VERSION=%d NR_OF_SECTIONS=%d\n", MAGIC, HEADER_SIZE, VERSION, (int)NR_OF_SECTIONS);
     bool flagMagic = false, flagVersion = false, flagSectNr = false, flagSectTypes = false;
     flagMagic = (MAGIC == 'S');
     flagVersion = (61 <= VERSION && 127 >= VERSION);
@@ -356,88 +355,7 @@ void parse(const char *path, int nr, char **option)
     return;
 }
 
-void list(const char *path, int nr, char **option)
-{
 
-    DIR *dir = NULL;
-    struct dirent *entry = NULL;
-    char fullPath[512];
-    struct stat statbuf;
-    bool flagRecursiv = false, flagSize = false, flagPerm = false;
-    long sizeConstrain = 0;
-    for (int i = 0; i < nr; i++)
-    {
-        if (strcmp(option[i], "recursive") == 0)
-        {
-            flagRecursiv = true;
-        }
-        else if (strstr(option[i], "size_smaller=") == option[i])
-        {
-            sscanf(option[i], "size_smaller=%ld", &sizeConstrain);
-            flagSize = true;
-        }
-        else if (strstr(option[i], "has_perm_write") == option[i])
-        {
-            flagPerm = true;
-        }
-    }
-    dir = opendir(path);
-    if (dir == NULL)
-    {
-        perror("Could not open directory");
-        return;
-    }
-    while ((entry = readdir(dir)) != NULL)
-    {
-        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
-        {
-            snprintf(fullPath, 512, "%s/%s", path, entry->d_name);
-            if (lstat(fullPath, &statbuf) == 0)
-            {
-
-                if (flagSize)
-                {
-                    if (S_ISREG(statbuf.st_mode))
-                    {
-                        if (sizeConstrain > statbuf.st_size)
-                        {
-                            if (!flagPerm)
-                            {
-                                printf("%s\n", fullPath);
-                            }
-                            else
-                            {
-                                if (statbuf.st_mode & __S_IWRITE)
-                                {
-                                    printf("%s\n", fullPath);
-                                }
-                            }
-                        }
-                    }
-                }
-                else if (flagPerm)
-                {
-                    if (statbuf.st_mode & __S_IWRITE)
-                    {
-                        printf("%s\n", fullPath);
-                    }
-                }
-                else
-                {
-                    printf("%s\n", fullPath);
-                }
-                if (flagRecursiv)
-                {
-                    if (S_ISDIR(statbuf.st_mode))
-                    {
-                        list(fullPath, nr, option);
-                    }
-                }
-            }
-        }
-    }
-    closedir(dir);
-}
 
 int main(int argc, char **argv)
 {
@@ -447,16 +365,6 @@ int main(int argc, char **argv)
         if (strcmp(argv[1], "variant") == 0)
         {
             printf("84112\n");
-        }
-        else if (strcmp(argv[1], "list") == 0)
-        {
-            if (sscanf(argv[argc - 1], "path=%s", c) == 1)
-            {
-                printf("SUCCESS\n");
-                list(c, argc - 3, (argv + 2));
-            }
-            else
-                puts("Invalid directory path!(last argument)");
         }
         else if (strcmp(argv[1], "parse") == 0)
         {
