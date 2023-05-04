e61a23439f265ea9f4468180a7c0b07e99c58268
ToByDaNy <cacarazatobias@yahoo.com>
2023-05-04 21:50:05 +0300

    ...

--- a2/a2.c
+++ a2/a2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <semaphore.h>
@@ -20,139 +20,140 @@
 
 void init();
 int info(int action, int processNr, int threadNr);
-sem_t* first;
+sem_t *first;
 
-void *function(void* arg)
+void *function(void *arg)
 {
     return NULL;
 }
 
-
-int main(){
+int main()
+{
     init();
     int nrProcese = 8;
     int nrThreads = 2;
-    pid_t* procese = (pid_t*)calloc(nrProcese,sizeof(pid_t));
-    //int parent = getpid();
+    pid_t *procese = (pid_t *)calloc(nrProcese, sizeof(pid_t));
     procese[0] = getpid();
-        //parent = getpid();
-    pthread_t* threads = (pthread_t*)calloc(nrThreads,sizeof(pthread_t)); 
+    pthread_t *threads = (pthread_t *)calloc(nrThreads, sizeof(pthread_t));
     info(BEGIN, 1, 0);
     procese[1] = fork();
-    if(procese[1] == -1)
+    if (procese[1] == -1)
     {
         return -1;
     }
-    else if(procese[1] == 0)
+    else if (procese[1] == 0)
     {
-        info(BEGIN,2,0);
+        info(BEGIN, 2, 0);
         procese[2] = fork();
-        if(procese[2] == -1)
+        if (procese[2] == -1)
         {
             return -1;
         }
-        else if(procese[2] == 0)
+        else if (procese[2] == 0)
         {
             info(BEGIN, 3, 0);
-            info(END,3,0);
+            info(END, 3, 0);
         }
-        else{
+        else
+        {
             procese[3] = fork();
-            if(procese[3] == -1)
+            if (procese[3] == -1)
             {
                 return -1;
             }
-            else if(procese[3] == 0)
+            else if (procese[3] == 0)
             {
                 info(BEGIN, 4, 0);
-                procese[5] =fork();
-                if(procese[5] == -1)
+                procese[5] = fork();
+                if (procese[5] == -1)
                 {
                     return -1;
                 }
-                else if(procese[5] == 0)
+                else if (procese[5] == 0)
                 {
-                    info(BEGIN,6,0);
-                    info(END,6,0);
+                    info(BEGIN, 6, 0);
+                    info(END, 6, 0);
                 }
                 else
                 {
                     procese[6] = fork();
-                    if(procese[6] == -1)
+                    if (procese[6] == -1)
                     {
                         return -1;
                     }
-                    else if(procese[6] == 0)
+                    else if (procese[6] == 0)
                     {
-                        info(BEGIN,7,0);
-                        info(END,7,0);
-                    }
-                    else
-                    {
+                        info(BEGIN, 7, 0);
                         procese[7] = fork();
-                        if(procese[7] == -1)
+                        if (procese[7] == -1)
                         {
                             return -1;
                         }
-                        else if(procese[7] == 0)
+                        else if (procese[7] == 0)
                         {
                             info(BEGIN, 8, 0);
-                            info(END,8,0);
+                            info(END, 8, 0);
                         }
                         else
                         {
-                            info(END,4,0);
+                            waitpid(procese[7], NULL, 0);
+                            info(END, 7, 0);
                         }
-
+                    }
+                    else
+                    {
+                        waitpid(procese[5], NULL, 0);
+                        waitpid(procese[6], NULL, 0);
+                        info(END, 4, 0);
                     }
                 }
-                
             }
             else
             {
                 procese[4] = fork();
-                if(procese[4] == -1)
+                if (procese[4] == -1)
                 {
                     return -1;
                 }
-                else if(procese[4] == 0)
+                else if (procese[4] == 0)
                 {
                     info(BEGIN, 5, 0);
-                    info(END,5 ,0);
+                    info(END, 5, 0);
                 }
-                else{
+                else
+                {
                     procese[8] = fork();
-                    if(procese[8] == -1)
+                    if (procese[8] == -1)
                     {
                         return -1;
                     }
-                    else if(procese[8] == 0)
+                    else if (procese[8] == 0)
                     {
-                        info(BEGIN,9,0);
-                        info(END,9, 0);
+                        info(BEGIN, 9, 0);
+                        info(END, 9, 0);
                     }
-                    else{
-                        info(END,2,0);
+                    else
+                    {
+                        waitpid(procese[2], NULL, 0);
+                        waitpid(procese[3], NULL, 0);
+                        waitpid(procese[4], NULL, 0);
+                        waitpid(procese[8], NULL, 0);
+                        info(END, 2, 0);
                     }
                 }
             }
         }
-        
-
     }
-    else{
-            info(END, 1, 0);
-    }
-    
-    for(int i = 0 ; i < nrProcese; i ++)
+    else
     {
-        waitpid(procese[i],NULL,0);
+
+        waitpid(procese[1], NULL, 0);
+
+        info(END, 1, 0);
     }
 
+    pthread_create(&(threads[0]), NULL, function, NULL);
 
-
-    pthread_create(&(threads[0]),NULL,function,NULL);
-    
     free(procese);
     free(threads);
 
